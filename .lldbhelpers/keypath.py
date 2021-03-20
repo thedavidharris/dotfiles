89fee677f2cc3524deeb8fef666408478016bab2
David Harris <dharr419@ford.com>
2021-03-20 12:00:12 -0400

    update lldb things

--- .lldbhelpers/keypath.py
+++ .lldbhelpers/keypath.py
@@ -1,95 +1,88 @@
+import aslr
 import lldb
 
 
-class KeypathException(Exception):
-    pass
+def _address_for_symbol(target, symbol_context, include_symbol):
+    symbol = symbol_context.GetSymbol()
+    function = symbol_context.GetFunction()
+    start_address = None
+    name = None
+
+    # Try to fetch the symbol's address first, and fallback to the function's
+    # It seems as though symbols contexts fetched with the "full" option (at
+    # least for swift symbols) contain an invalid symbol, but a valid function,
+    # yet functions fetched in other ways, have the opposite, a valid symbol,
+    # and invalid function. Now we check to see if they're valid, fetching the
+    # necessary properties for both, and fallback to failing entirely
+    if symbol.IsValid():
+        start_address = symbol.GetStartAddress()
+        name = symbol.GetName()
+    elif function.IsValid():
+        start_address = function.GetStartAddress()
+        name = function.GetDisplayName()
+    else:
+        raise Exception("Neither function nor symbol is valid")
+
+    if not start_address.IsValid():
+        raise Exception("Start address is invalid")
+
+    module = symbol_context.GetModule()
+    offset = aslr.aslr_for_module(target, module)
+    address = hex(offset + start_address.GetFileAddress())
+
+    if include_symbol:
+        return "{}: {}".format(name, address)
+    else:
+        return "{}".format(address)
 
 
-def _run_command(debugger, command):
-    interpreter = debugger.GetCommandInterpreter()
-    result = lldb.SBCommandReturnObject()
-    interpreter.HandleCommand(command, result)
-
-    if not result.Succeeded():
-        raise KeypathException(result.GetError())
-
-
-def _is_swift_context(context):
-    language = context.GetFrame().GetFunction().GetLanguage()
-    return language == lldb.eLanguageTypeSwift
-
-
-def _swift_statement_for_target(target):
-    if target.startswith("0x"):
-        return "unsafeBitCast({}, to: NSObject.self)".format(target)
-
-    return target
-
-
-def _run_swift_command(debugger, result, target, path):
-    swift_command = '{}.value(forKeyPath: "{}")'.format(
-        _swift_statement_for_target(target), path
-    )
-
-    debugger_command = "po "
-    debugger_command += "if let value = %s {" % swift_command
-    debugger_command += "    print(value)"
-    debugger_command += "} else {"
-    debugger_command += '    print("nil")'
-    debugger_command += "}"
-
-    try:
-        _run_command(debugger, debugger_command)
-        result.SetStatus(lldb.eReturnStatusSuccessFinishResult)
-    except KeypathException as e:
-        result.AppendWarning(e.message)
-        result.SetStatus(lldb.eReturnStatusFailed)
-
-
-def _run_objc_command(debugger, result, target, path):
-    objc_command = '[(id){} valueForKeyPath:@"{}"]'.format(target, path)
-    debugger_command = "po "
-    debugger_command += "id value = {};".format(objc_command)
-    debugger_command += 'printf("%s\\n",'
-    debugger_command += "[(NSString *)[value debugDescription]"
-    debugger_command += "                    UTF8String]);"
-
-    try:
-        _run_command(debugger, debugger_command)
-        result.SetStatus(lldb.eReturnStatusSuccessFinishResult)
-    except KeypathException as e:
-        result.AppendWarning(e.message)
-        result.SetStatus(lldb.eReturnStatusFailed)
-
-
-def keypath(debugger, command, context, result, internal_dict):
+@lldb.command()
+def afs(debugger, command, context, result, _):
     """
-    Print the value for the given keypath on the given object from Objective-C
-    or Swift
+    Get the address for a symbol regardless of the ASLR offset
 
-    (lldb) keypath self.foo.bar
-      output or nil
+    (lldb) afs viewDidLoad
+    -[UIViewController viewDidLoad]: 0x1146c79a4
+    ...
 
-    (lldb) keypath 0x1234.foo.bar
-      output or nil
+    (lldb) afs -[MyViewController viewDidLoad]:
+    0x1146c79a4
     """
-    command = command.strip()
-
-    if "." not in command:
-        result.AppendWarning("Invalid command, must contain `.`")
+    # TODO: Allow user to pass module for symbol
+    symbol = command.strip()
+    if "?" in symbol:
+        result.AppendWarning(
+            "Symbol must not have Swift syntax sugar '{}'".format(symbol)
+        )
         result.SetStatus(lldb.eReturnStatusFailed)
         return
 
-    parts = command.split(".", 1)
-    target = parts[0].strip()
-    path = parts[1].strip()
+    target = debugger.GetSelectedTarget()
 
-    if _is_swift_context(context):
-        _run_swift_command(debugger, result, target, path)
+    # Search for functions with both the original, and auto, arguments
+    # The default to this argument is "any" yet it seems as though you
+    # don't actually search for any. For example:
+    # argument | swift symbol                               | result
+    # ---------+--------------------------------------------+-------------
+    # any      | ViewController.viewDidLoad                 | valid symbol
+    # any      | module.ViewController.viewDidLoad () -> () | none
+    # full     | ViewController.viewDidLoad                 | none
+    # full     | module.ViewController.viewDidLoad () -> () | valid symbol
+    #
+    # In this case, I would expect any + a full symbol to still return
+    # the full symbol. This is also supposed to be a bitwise field, so I've
+    # also tried passing multiple options, with no success
+    functions = target.FindFunctions(symbol, lldb.eFunctionNameTypeFull)
+    for symbol_context in target.FindFunctions(symbol):
+        functions.Append(symbol_context)
+
+    if len(functions) == 0:
+        result.AppendWarning("No symbol found for '{}'".format(symbol))
+        result.SetStatus(lldb.eReturnStatusFailed)
+    elif len(functions) == 1:
+        result.AppendMessage(_address_for_symbol(target, functions[0], False))
+        result.SetStatus(lldb.eReturnStatusSuccessFinishResult)
     else:
-        _run_objc_command(debugger, result, target, path)
-
-
-def __lldb_init_module(debugger, internal_dict):
-    handle = debugger.HandleCommand
-    handle("command script add -f keypath.keypath keypath")
+        for context in functions:
+            result.AppendMessage(_address_for_symbol(target, context, True))
+        result.SetStatus(lldb.eReturnStatusSuccessFinishResult)
