aea83011c750dd718c7802041d5cf0711bb7f2c8
David Harris <davidaharris@outlook.com>
2025-02-04 10:36:34 -0500

    updates

--- dot_config/zsh/conf.d/env.zsh
+++ dot_config/zsh/conf.d/env.zsh
@@ -1,22 +1,54 @@
-if [[ $OSTYPE == darwin* ]]; then
-  export OSX=true
+# Set Zsh options related to globbing.
+setopt extended_glob         # Use more awesome globbing features.
+setopt NO_rm_star_silent     # Ask for confirmation for `rm *' or `rm path/*'
+
+# Set general Zsh options.
+setopt combining_chars       # Combine 0-len chars with the base character (eg: accents).
+setopt interactive_comments  # Enable comments in interactive shell.
+setopt rc_quotes             # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
+setopt NO_mail_warning       # Don't print a warning message if a mail file has been accessed.
+setopt NO_beep               # Don't beep on error in line editor.
+
+# Set Zsh options related to job control.
+setopt auto_resume           # Attempt to resume existing job before creating a new process.
+setopt long_list_jobs        # List jobs in the long format by default.
+setopt notify                # Report status of background jobs immediately.
+setopt NO_bg_nice            # Don't run all background jobs at a lower priority.
+setopt NO_check_jobs         # Don't report on jobs when shell exit.
+setopt NO_hup                # Don't kill jobs on shell exit.
+
+# Set XDG base dirs.
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
+mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_STATE_HOME
+
+
+# Set browser.
+if [[ "$OSTYPE" == darwin* ]]; then
+  export BROWSER=${BROWSER:-open}
 fi
 
-# Make history a bit bigger than macOS default.
-SAVEHIST=2000
-HISTSIZE=$(( 1.2 * SAVEHIST))  # Zsh recommended value
+# Set language.
+export LANG=${LANG:-en_US.UTF-8}
+
+# Reduce key delay
+export KEYTIMEOUT=${KEYTIMEOUT:-1}
+
+# Make Apple Terminal behave.
+if [[ "$OSTYPE" == darwin* ]]; then
+  export SHELL_SESSIONS_DISABLE=${SHELL_SESSIONS_DISABLE:-1}
+fi
+
+# Use `< file` to quickly view the contents of any file.
+[[ -z "$READNULLCMD" ]] || READNULLCMD=$PAGER
 
 # Set default editor to Neovim
 export EDITOR=nvim
 
 export VISUAL=code
-
-# Disable ^-S in the terminal
-stty -ixon -ixoff
-
-# Avoid issues with `gpg` as installed via Homebrew.
-# https://stackoverflow.com/a/42265848/96656
-export GPG_TTY=$(tty)
 
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 export PYTHONIOENCODING='UTF-8';
@@ -28,7 +60,7 @@ export LC_ALL='en_US.UTF-8';
 export CLICOLOR=1
 
 export NVM_DIR=$HOME/nvm
-export GRADLE_USER_HOME=$HOME/gradle
+export GRADLE_USER_HOME=$XDG_CONFIG_HOME/gradle
 
 export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"  
 
