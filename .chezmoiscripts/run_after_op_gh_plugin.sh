4af9340dafb9ee276b6388171e2c67367c8d1956
David Harris <davidaharris@outlook.com>
2025-10-22 13:38:08 -0400

    Update gitconfig

--- .chezmoiscripts/run_after_op_gh_plugin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f ~/.config/op/plugins/gh.json ]; then
-    # Initialize the op gh plugin for the default (personal)
-    echo "Set up op gh plugin..."
-    cd $HOME
-    op plugin init gh
-fi
